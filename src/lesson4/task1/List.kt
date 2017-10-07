fc29d157243743aadb6f7e09a08b7e3be0cae805
AlexandrovaIrina <alexandrovairina@mail.ru>
2017-10-07 16:50:44 +0300

    Решены часть задач lesson4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -229,11 +229,7 @@ fun factorize(n: Int): List<Int> {
  * Разложить заданное натуральное число n > 1 на простые множители.
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  */
-fun factorizeToString(n: Int): String{
-    var listOfNumber = factorize(n)
-    var ans = listOfNumber.joinToString(separator = "*")
-    return ans
-}
+fun factorizeToString(n: Int): String = TODO()
 
 /**
  * Средняя
