6676d1853ab7dca59857e877786d3c283c215a35
AlexandrovaIrina <alexandrovairina@mail.ru>
2017-12-04 19:40:26 +0300

    Исправлены задачи с учетом комментариев

--- src/lesson7/task2/Matrices.kt
+++ src/lesson7/task2/Matrices.kt
@@ -78,7 +78,7 @@ fun generateSpiral(height: Int, width: Int): Matrix<Int> {
         while(row > fromRow && column >= fromColumn) ans[row, column--] = value++
         column++
         row--
-        while(row > fromRow && ans[row, column] != 0) ans[row--, column] = value++
+        while(row > fromRow && ans[row, column] == 0) ans[row--, column] = value++
         fromRow++
         toRow--
         fromColumn++
